38f4fac908952d47a4dfd7a015bd223933fdec55
48-iq <ivn25ov@mail.ru>
2025-07-30 07:43:35 +0400

    fix tests

--- src/test/java/com/example/botFSSP/BotFsspApplicationTests.java
+++ src/test/java/com/example/botFSSP/BotFsspApplicationTests.java
@@ -3,7 +3,6 @@ package com.example.botFSSP;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BotFsspApplicationTests {
 
 	@Test
